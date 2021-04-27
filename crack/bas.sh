2d62bfcf709438f0f9efc3b9e0e0bb57c8ea2710
Michal D <154359+mdziczkowski@users.noreply.github.com>
2021-04-27 22:41:25 +0200

    Adding files of Sublime 3

---
+++ crack/bas.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo -e "
+
+# sublime block
+
+127.0.0.1 www.sublimetext.com
+127.0.0.1 sublimetext.com
+127.0.0.1 sublimehq.com
+127.0.0.1 telemetry.sublimehq.com
+127.0.0.1 l127.0.0.1 www.sublimetext.com
+127.0.0.1 sublimetext.com
+127.0.0.1 sublimehq.com
+127.0.0.1 telemetry.sublimehq.com
+127.0.0.1 license.sublimehq.com
+127.0.0.1 45.55.255.55
+127.0.0.1 45.55.41.223
+0.0.0.0 license.sublimehq.com
+0.0.0.0 45.55.255.55
+0.0.0.0 45.55.41.223
+127.0.0.1 45.55.255.55
+127.0.0.1 45.55.41.223
+
+0.0.0.0 license.sublimehq.com
+0.0.0.0 45.55.255.55
+0.0.0.0 45.55.41.223
+" >> /etc/hosts
